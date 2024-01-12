18318db19ba7b9c5782997688ce4e8b56d568016
LEONARDO PEREIRA <30580018+LeonardoPereirajr@users.noreply.github.com>
2024-01-11 21:28:12 -0300

    Botao

--- sorteio_app/urls.py
+++ sorteio_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import sorteio, login
+from . import views
 
 urlpatterns = [
     path('sorteio/', sorteio, name='sorteio'),
     path('login/', login, name='login'),
+     path('sorteio_upload/', views.sorteio_upload, name='sorteio_upload'),
 ]
--- sorteio_app/views.py
+++ sorteio_app/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render
 import csv
 import random
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
 def sorteio(request):
-    arquivo_csv = 'C:/Users/leojr/Downloads/sorteio_projeto/resultado.csv'  # Substitua pelo caminho correto
-    with open(arquivo_csv, 'r', encoding='utf-8') as csvfile:
-        leitor_csv = csv.reader(csvfile)
-        nomes = [linha[0] for linha in leitor_csv]
-
-    if nomes:
-        ganhador = random.choice(nomes)
-    else:
-        ganhador = 'Não há nomes para sortear.'
-
+    ganhador = request.session.get('ganhador', None)
     return render(request, 'sorteio_app/sorteio.html', {'ganhador': ganhador})
 
 def login(request):
     return render(request, 'sorteio_app/login.html')
+
+def sorteio_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+
+        # Salva o arquivo no sistema temporário
+        fs = FileSystemStorage(location='/tmp')
+        filename = fs.save(myfile.name, myfile)
+
+        # Caminho do arquivo salvo
+        arquivo_csv = fs.url(filename)
+
+        # Realiza o sorteio com o arquivo CSV
+        with open(fs.path(filename), 'r', encoding='utf-8') as csvfile:
+            leitor_csv = csv.reader(csvfile)
+            nomes = [linha[0] for linha in leitor_csv]
+
+        if nomes:
+            ganhador = random.choice(nomes)
+            request.session['ganhador'] = ganhador
+        else:
+            ganhador = 'Não há nomes para sortear.'
+        
+    return sorteio(request)
